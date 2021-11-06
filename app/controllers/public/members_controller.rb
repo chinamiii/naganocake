08da992f6b6555a5dabeebef1b609ca319c45c87
chinamiii <chinami.2929@icloud.com>
2021-11-06 15:21:32 +0000

    [edit miss]

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -5,9 +5,15 @@ class Public::MembersController < ApplicationController
   end
 
   def edit
+    @memmber = current_member
   end
 
+
+
   def update
+    @member = current_member
+    @member.updata(member_params)
+    redirect_to public_mypage_path(@member)
   end
 
   def unsubscribe
@@ -16,10 +22,10 @@ class Public::MembersController < ApplicationController
   def withdrawal
   end
 
- private
+  private
 
-  def member_params
-    params.require(:member).permit(:last_name, :first_name, :last_name_kana, :last_name_kana, :postal_code, :address, :telephone_number, :email)
-  end
+  	def member_params
+  		params.require(:member).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :address, :postal_code, :phone_number, :email, :is_deleted)
+  	end
 
 end
