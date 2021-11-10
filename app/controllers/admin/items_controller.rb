e9bc3c6507b5d445058284c9b7c965ea7d6f0208
chinamiii <chinami.2929@icloud.com>
2021-11-10 15:22:42 +0000

    [item new]

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -5,12 +5,18 @@ class Admin::ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path(@item.id)
+
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
@@ -18,4 +24,11 @@ class Admin::ItemsController < ApplicationController
 
   def update
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :body, :image, :price, :is_active, :genre_id)
+  end
+  
 end
